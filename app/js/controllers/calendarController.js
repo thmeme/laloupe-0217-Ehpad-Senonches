ea3a8ba125645ad9ad67749e064ed6e958a851c0
thmeme <thierrymeme@gmail.com>
2017-04-26 17:20:22 +0200

    CSS Submenus news and calendar

--- app/js/controllers/calendarController.js
+++ app/js/controllers/calendarController.js
@@ -1,33 +1,38 @@
 angular.module('app')
-  .controller('CalendarController',
-    function($scope, CurrentUser, CalendarService) {
-      $scope.user = CurrentUser.user();
+    .controller('CalendarController',
+        function($scope, CurrentUser, CalendarService) {
+            $scope.user = CurrentUser.user();
 
-      CalendarService.getAll().then(function(res) {
-        $scope.evenements = res.data;
-      });
+            CalendarService.getAll().then(function(res) {
+                $scope.evenements = res.data;
+            });
 
-      $scope.addEvenement = function() {
-        $scope.evenements = [];
-        let evenement = {
-          date: $scope.myDate +'z',
-          start: $scope.start,
-          end: $scope.end,
-          title: $scope.title,
-          content: $scope.content
-        };
-        CalendarService.create(evenement).then(function(res) {
-          console.log(evenement, res);
-          $scope.myDate = '';
-          $scope.start = '';
-          $scope.end = '';
-          $scope.title = '';
-          $scope.content = '';
-        }, function(err) {
-          console.log('echec');
+            $scope.addEvenement = function() {
+                $scope.evenements = [];
+                let evenement = {
+                    date: $scope.myDate + 'z',
+                    start: $scope.start,
+                    end: $scope.end,
+                    title: $scope.title,
+                    content: $scope.content
+                };
+                CalendarService.create(evenement).then(function(res) {
+                    console.log(evenement, res);
+                    $scope.myDate = '';
+                    $scope.start = '';
+                    $scope.end = '';
+                    $scope.title = '';
+                    $scope.content = '';
+                    loadEvents();
+                }, function(err) {
+                    console.log('echec');
+                });
+
+                function loadEvents() {
+                    CalendarService.getAll().then(function(res) {
+                        $scope.evenements = res.data;
+                    });
+                }
+                loadEvents();
+            };
         });
-        CalendarService.getAll().then(function(res) {
-          $scope.evenements = res.data;
-        });
-      };
-    });
