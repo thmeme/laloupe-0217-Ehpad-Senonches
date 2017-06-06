464bf148c66a30443e965412fc7bf2f082d7fa92
thmeme <thierrymeme@gmail.com>
2017-06-06 13:10:34 +0200

    create slideshows files

--- app/js/controllers/slideshowController.js
+++ app/js/controllers/slideshowController.js
@@ -1,84 +1,84 @@
-angular.module('app')
-  .controller('SlideshowController', function($scope, $stateParams, $window, $state, SlideshowService, UploadPdfService, UploadService, $timeout, $mdDialog, CurrentUser, newsService) {
-    $scope.user = CurrentUser.user();
-
-    $scope.idImg = $stateParams.id;
-    console.log('id', $scope.idImg);
-
-    $scope.showConfirm = function(ev, id) {
-      // Appending dialog to document.body to cover sidenav in docs app
-      var confirm = $mdDialog.confirm()
-        .title('Voulez vous supprimer cet article ?')
-        .textContent('Tous les éléments seront définitivement perdus')
-        .ariaLabel('Lucky day')
-        .targetEvent(ev)
-        .ok('Supprimer')
-        .cancel('Annuler');
-
-      $mdDialog.show(confirm).then(function() {
-        newsService.delete(id).then(function(res) {
-          loadAllNews();
-        });
-      });
-    };
-
-    $scope.UploadImgModalShown = false;
-    $scope.OpenModalUploadImg = function() {
-      $scope.UploadImgModalShown = !$scope.UploadImgModalShown;
-    };
-
-    $scope.galleryInsertModalShown = false;
-    $scope.OpenModalgalleryInsert = function() {
-      $scope.galleryInsertModalShown = !$scope.galleryInsertModalShown;
-      UploadService.getAll().then(function(res) {
-        console.log('load', res);
-        $scope.listimages = res.data;
-      }, function(err) {
-        console.error('error on image load', err);
-      });
-    };
-
-    $scope.currentPageNews = 0;
-    $scope.pageSizeNews = 5;
-    $scope.listNews = [];
-    $scope.numberOfPagesNews = function() {
-      return Math.ceil($scope.listNews.length / $scope.pageSizeNews);
-    };
-    for (var i = 0; i < $scope.listNews.length - 1; i++) {
-      $scope.listNews.push("Item " + i);
-    }
-
-    $scope.galleryAssociateModalShown = false;
-    $scope.OpenModalgalleryAssociate = function() {
-        $scope.galleryAssociateModalShown = !$scope.galleryAssociateModalShown;
-        UploadService.getAll().then(function(res) {
-          console.log('load', res);
-          $scope.listimages = res.data;
-        }, function(err) {
-          console.error('error on image load', err);
-        });
-    };
-
-    // $scope.newImgSlideshow = {
-    //   id:'',
-    //   name: '',
-    //   status: '',
-    // };
-    $scope.addSlideShow = function (nameImg) {
-      SlideshowService.create(nameImg).then(function(res) {
-        console.log('newImgSlideshow', res);
-
-      });
-    };
-
-
-    $scope.currentPage = 0;
-    $scope.pageSize = 12;
-    $scope.listimages = [];
-    $scope.numberOfPages = function() {
-      return Math.ceil($scope.listimages.length / $scope.pageSize);
-    };
-    for (var i = 0; i < $scope.listimages.length - 1; i++) {
-      $scope.listimages.push("Item " + i);
-    }
-  });
+// angular.module('app')
+//   .controller('SlideshowController', function($scope, $stateParams, $window, $state, SlideshowService, UploadPdfService, UploadService, $timeout, $mdDialog, CurrentUser, newsService) {
+//     $scope.user = CurrentUser.user();
+//
+//     $scope.idImg = $stateParams.id;
+//     console.log('id', $scope.idImg);
+//
+//     $scope.showConfirm = function(ev, id) {
+//       // Appending dialog to document.body to cover sidenav in docs app
+//       var confirm = $mdDialog.confirm()
+//         .title('Voulez vous supprimer cet article ?')
+//         .textContent('Tous les éléments seront définitivement perdus')
+//         .ariaLabel('Lucky day')
+//         .targetEvent(ev)
+//         .ok('Supprimer')
+//         .cancel('Annuler');
+//
+//       $mdDialog.show(confirm).then(function() {
+//         newsService.delete(id).then(function(res) {
+//           loadAllNews();
+//         });
+//       });
+//     };
+//
+//     $scope.UploadImgModalShown = false;
+//     $scope.OpenModalUploadImg = function() {
+//       $scope.UploadImgModalShown = !$scope.UploadImgModalShown;
+//     };
+//
+//     $scope.galleryInsertModalShown = false;
+//     $scope.OpenModalgalleryInsert = function() {
+//       $scope.galleryInsertModalShown = !$scope.galleryInsertModalShown;
+//       UploadService.getAll().then(function(res) {
+//         console.log('load', res);
+//         $scope.listimages = res.data;
+//       }, function(err) {
+//         console.error('error on image load', err);
+//       });
+//     };
+//
+//     $scope.currentPageNews = 0;
+//     $scope.pageSizeNews = 5;
+//     $scope.listNews = [];
+//     $scope.numberOfPagesNews = function() {
+//       return Math.ceil($scope.listNews.length / $scope.pageSizeNews);
+//     };
+//     for (var i = 0; i < $scope.listNews.length - 1; i++) {
+//       $scope.listNews.push("Item " + i);
+//     }
+//
+//     $scope.galleryAssociateModalShown = false;
+//     $scope.OpenModalgalleryAssociate = function() {
+//         $scope.galleryAssociateModalShown = !$scope.galleryAssociateModalShown;
+//         UploadService.getAll().then(function(res) {
+//           console.log('load', res);
+//           $scope.listimages = res.data;
+//         }, function(err) {
+//           console.error('error on image load', err);
+//         });
+//     };
+//
+//     // $scope.newImgSlideshow = {
+//     //   id:'',
+//     //   name: '',
+//     //   status: '',
+//     // };
+//     $scope.addSlideShow = function (nameImg) {
+//       SlideshowService.create(nameImg).then(function(res) {
+//         console.log('newImgSlideshow', res);
+//
+//       });
+//     };
+//
+//
+//     $scope.currentPage = 0;
+//     $scope.pageSize = 12;
+//     $scope.listimages = [];
+//     $scope.numberOfPages = function() {
+//       return Math.ceil($scope.listimages.length / $scope.pageSize);
+//     };
+//     for (var i = 0; i < $scope.listimages.length - 1; i++) {
+//       $scope.listimages.push("Item " + a);
+//     }
+//   });
--- server/api/models/slideshow.js
+++ server/api/models/slideshow.js
@@ -1,112 +1,112 @@
-import mongoose from 'mongoose';
-import submenu from './slideshow.js';
-
-const imgslideshowSchema = new mongoose.Schema({
-
-    id: {
-        type: String,
-    },
-    name: {
-        type: String,
-    },
-    status: {
-      type: Boolean,
-      default: true,
-    }
-});
-let model = mongoose.model('imgslideshow', newsSchema);
-
-export default class News {
-
-    findAll(req, res) {
-        model.find({},
-            (err, news) => {
-                if (err || !news) {
-                    res.sendStatus(403);
-                } else {
-                    res.json(news);
-                }
-            });
-    }
-    findById(req, res) {
-        console.log('req in back', req.params, req.body, req.query);
-        model.findById(req.params.id, function(err, news) {
-          if (err || !news) {
-              res.sendStatus(403);
-          } else {
-              res.json(news);
-          }
-        });
-    }
-    create(req, res) {
-        let news = req.body;
-        console.log('back', req.body);
-        news.date = new Date().toISOString();
-        model.create(news, (err, news) => {
-            if (err) {
-                res.status(500).send({
-                    error: err
-                });
-            } else {
-                res.json({
-                    success: true,
-                    news: news
-                });
-            }
-        });
-    }
-    createByUser(req, res) {
-        let news = req.body;
-        console.log('back', req.body);
-        news.date = new Date().toISOString();
-        delete submenu.isOnline;
-        model.create(news, (err, news) => {
-            if (err) {
-                res.status(500).send({
-                    error: err
-                });
-            } else {
-                res.json({
-                    success: true,
-                    news: news
-                });
-            }
-        });
-    }
-    update(req, res) {
-        model.findByIdAndUpdate(req.params.id,
-          req.body, { new: true },function(err, news) {
-            if (err) {
-              res.status(500).send(err);
-            } else {
-              res.json({
-                  success: true,
-                  news: news
-              });
-            }
-        });
-    }
-    updateByUser(req, res) {
-      delete submenu.isOnline;
-        model.findByIdAndUpdate(req.params.id,
-          req.body, { new: true },function(err, news) {
-            if (err) {
-              res.status(500).send(err);
-            } else {
-              res.json({
-                  success: true,
-                  news: news
-              });
-            }
-        });
-    }
-    delete(req, res) {
-        model.findByIdAndRemove(req.params.id, function(err) {
-            if (err) {
-                res.status(500).send(err.message);
-            } else {
-                res.sendStatus(200);
-            }
-        });
-    }
-}
+// import mongoose from 'mongoose';
+// import slideshow from './slideshow.js';
+//
+// const imgslideshowSchema = new mongoose.Schema({
+//
+//     id: {
+//         type: String,
+//     },
+//     name: {
+//         type: String,
+//     },
+//     status: {
+//       type: Boolean,
+//       default: true,
+//     }
+// });
+// let model = mongoose.model('imgslideshow', imgslideshowSchema);
+//
+// export default class Imgslideshow {
+//
+//     findAll(req, res) {
+//         model.find({},
+//             (err, news) => {
+//                 if (err || !news) {
+//                     res.sendStatus(403);
+//                 } else {
+//                     res.json(news);
+//                 }
+//             });
+//     }
+//     findById(req, res) {
+//         console.log('req in back', req.params, req.body, req.query);
+//         model.findById(req.params.id, function(err, news) {
+//           if (err || !news) {
+//               res.sendStatus(403);
+//           } else {
+//               res.json(news);
+//           }
+//         });
+//     }
+//     create(req, res) {
+//         let news = req.body;
+//         console.log('back', req.body);
+//         news.date = new Date().toISOString();
+//         model.create(news, (err, news) => {
+//             if (err) {
+//                 res.status(500).send({
+//                     error: err
+//                 });
+//             } else {
+//                 res.json({
+//                     success: true,
+//                     news: news
+//                 });
+//             }
+//         });
+//     }
+//     createByUser(req, res) {
+//         let news = req.body;
+//         console.log('back', req.body);
+//         news.date = new Date().toISOString();
+//         delete submenu.isOnline;
+//         model.create(news, (err, news) => {
+//             if (err) {
+//                 res.status(500).send({
+//                     error: err
+//                 });
+//             } else {
+//                 res.json({
+//                     success: true,
+//                     news: news
+//                 });
+//             }
+//         });
+//     }
+//     update(req, res) {
+//         model.findByIdAndUpdate(req.params.id,
+//           req.body, { new: true },function(err, news) {
+//             if (err) {
+//               res.status(500).send(err);
+//             } else {
+//               res.json({
+//                   success: true,
+//                   news: news
+//               });
+//             }
+//         });
+//     }
+//     updateByUser(req, res) {
+//       delete submenu.isOnline;
+//         model.findByIdAndUpdate(req.params.id,
+//           req.body, { new: true },function(err, news) {
+//             if (err) {
+//               res.status(500).send(err);
+//             } else {
+//               res.json({
+//                   success: true,
+//                   news: news
+//               });
+//             }
+//         });
+//     }
+//     delete(req, res) {
+//         model.findByIdAndRemove(req.params.id, function(err) {
+//             if (err) {
+//                 res.status(500).send(err.message);
+//             } else {
+//                 res.sendStatus(200);
+//             }
+//         });
+//     }
+// }
