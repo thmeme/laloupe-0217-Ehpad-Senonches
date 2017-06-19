48291576d2b3c50a66f7a8cd6949643950e3bf7d
thmeme <thierrymeme@gmail.com>
2017-06-19 10:55:17 +0200

    test1 ok

---
+++ spec/userApi.spec.js
@@ -0,0 +1,55 @@
+const assert = require('assert');
+const request = require('supertest');
+
+let app = "http://localhost:3000",
+    admin,
+    admin_token,
+    user,
+    user_token;
+
+describe('API USER', function() {
+
+  it('should connect admin', function(done) {
+    request(app)
+      .post('/login')
+      .send({
+        email: "admin@mail.com",
+        password: '12345'
+      })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        admin = res.body.user;
+        admin_token = res.body.token;
+        assert.equal(admin.email, "admin@mail.com");
+        done();
+      });
+  });
+
+  it('admin should create a user', function(done) {
+    request(app)
+      .post('/users')
+      .set('Authorization', admin_token)
+      .send({
+        email: "user@mail.fr",
+        password: 'azerty',
+        Name: 'Test',
+        Firstname: 'test'
+      })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        user = res.body.user;
+        assert.equal(user.email, "user@mail.fr");
+        done();
+      });
+  });
+
+  it('admin should delete user', function(done) {
+    request(app)
+      .delete('/users/' + user._id)
+      .set('Authorization', admin_token)
+      .expect(200, done);
+  });
+
+});
