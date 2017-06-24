7ff12030908f2ad44331e704a51ab79da507cad2
thmeme <thierrymeme@gmail.com>
2017-06-24 18:56:44 +0200

    new theme color

--- app/js/consts/themeMaterial.js
+++ app/js/consts/themeMaterial.js
@@ -23,13 +23,12 @@ angular.module('app')
 
     $mdThemingProvider.definePalette('front', {
       '50': '568dbc',
-
       '100': 'ffcdd2',
       '200': 'ef9a9a',
       '300': 'e57373',
       '400': 'ef5350',
       '500': 'f44336',
-      '600': 'e53935',
+      '600': '15426A',
       '700': 'd32f2f',
       '800': 'c62828',
       '900': 'b71c1c',
