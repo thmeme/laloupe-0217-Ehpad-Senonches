c5552097fe35adad1c4fc0a5459cbf4bb19b0b7f
jordan-couard <jordan.couard@gmail.com>
2017-06-28 12:07:28 +0200

    addmodalMail

--- app/js/controllers/Anon/mailController.js
+++ app/js/controllers/Anon/mailController.js
@@ -34,22 +34,33 @@ angular.module('app')
 
 
     $scope.sendMail = function () {
-      console.log($scope.mail);
-      MailService.sendMail($scope.mail).then(function(res) {
-      });
-      $scope.mail = {
-        email:'',
-        name:'',
-        subject:'',
-        message:''
+        console.log($scope.mail);
+       swal(
+         'Envoi',
+         'Votre email est en cours d\'envoi',
+         'info'
+       );
+        MailService.sendMail($scope.mail).then(function(res) {
+         if (res.status === 200) {
+           swal(
+             'Succès !',
+             'Votre email a bien été envoyé!',
+             'success'
+           );
+           document.getElementById("send").innerHTML = "Mail envoyé";
+         } else {
+           swal(
+             'ERREUR !',
+             'L\'envoi de votre email a échoué!',
+             'error'
+           );
+         }
+        });
+        $scope.mail = {
+          email:'',
+          name:'',
+          subject:'',
+          message:''
+        };
       };
-      swal(
-         'Succès !',
-        'Votre email a bien été envoyé!',
-        'success'
-      );
-     };
-
-
-
   });
