597b76a7a0264215fbc7893c3023d46427493fdc
thmeme <thierrymeme@gmail.com>
2017-06-28 14:53:51 +0200

    create contact

---
+++ server/api/routes/contacts.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Contact from '../models/contact.js';
+import Auth from '../middlewares/authorization.js';
+
+let router = express.Router();
+
+module.exports = (app) => {
+
+  var contact = new Contact();
+
+  router.get('/', Auth.hasAuthorization, contact.findAllByUser);
+
+  router.get('/:id', contact.findById);
+
+  router.put('/admin/:id', Auth.isAdministrator, contact.update);
+  router.put('/:id', Auth.hasAuthorization, contact.updateByUser);
+
+  app.use('/welcome', router);
+};
