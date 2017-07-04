851cfd3e5c38f8369a29ead68fd60491401ad66f
thmeme <thierrymeme@gmail.com>
2017-07-04 14:18:23 +0200

    debug

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -33,6 +33,10 @@ const userSchema = new mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false
+    },
+    isSuperAdmin: {
+        type: Boolean,
+        default: false
     }
 });
 
